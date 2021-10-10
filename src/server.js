5d3abbbf75aeed7586c1516ba5dd67b1a4d8a0a2
thaibanghoang <thaibang1983hn@gmail.com>
2021-10-10 10:38:45 +0200

    middleware

--- src/server.js
+++ src/server.js
@@ -5,8 +5,20 @@ import initAPIRoute from "./route/api";
 
 require("dotenv").config();
 
+let morgan = require("morgan");
+
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+  // this middleware use for check the status
+  // if nothing wrong, the function next will be call
+  // and after that the code continue to run
+  // this is a simple example of a middleware
+  next();
+});
+
+app.use(morgan("combined"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +26,11 @@ app.use(express.json());
 configViewEngine(app); // setup view engine
 initWebRoute(app); // init web route: define the route for the website
 initAPIRoute(app); // init API route: start API of the website
+
+// hanld middleware 404 not found
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Node is available at: http://localhost:${port}`);
